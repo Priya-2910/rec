d38053ad2e20f0111f87b51c530c22422ec008cf
Priya-2910 <231901025@rajalakshmi.edu.in>
2025-09-17 12:18:36 +0530

    Add files via upload

---
+++ taskRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { createTask, getAllTasks, getTaskById } = require("../controllers/taskController");
+
+const router = express.Router();
+
+router.post("/", createTask);
+router.get("/", getAllTasks);
+router.get("/:id", getTaskById);
+
+module.exports = router;
